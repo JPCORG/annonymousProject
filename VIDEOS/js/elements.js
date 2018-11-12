7d30c1fb5a1e8306c4fc8c967f1b38d72a99ebcd
JPCYURI <44944272+JPCYURI@users.noreply.github.com>
2018-11-12 20:04:51 +0800

    Add files via upload

---
+++ VIDEOS/js/elements.js
@@ -0,0 +1,16 @@
+var parent = document.getElementById("gallery");
+var v1 = document.createElement("div");
+var v2 = document.createElement('div');
+v1.className = "gallery";
+v2.innerHTML = "CUTE PINAY QUICKY SEX";
+v2.className = "desc";
+var a = document.createElement("a");
+a.setAttribute("href", "vid1.html");
+
+var img = document.createElement("img");
+img.setAttribute('src', "https://img-egc.xnxx-cdn.com/videos/thumbslll/e2/e1/b7/e2e1b722b32ed5bd0a044d7e2a54c71d/e2e1b722b32ed5bd0a044d7e2a54c71d.27.jpg");
+
+a.appendChild(img);
+a.appendChild(v2);
+v1.appendChild(a);
+parent.appendChild(v1);
