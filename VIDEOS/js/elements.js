9e5a6bf44d5ea0ccbfdc818b6620c07ced66a9b2
JPCYURI <44944272+JPCYURI@users.noreply.github.com>
2018-11-12 20:50:34 +0800

    Add files via upload

--- VIDEOS/js/elements.js
+++ VIDEOS/js/elements.js
@@ -14,3 +14,22 @@ a.appendChild(img);
 a.appendChild(v2);
 v1.appendChild(a);
 parent.appendChild(v1);
+
+///////////////////////////////////////////////////////////
+
+var parent = document.getElementById("gallery");
+var v1 = document.createElement("div");
+var v2 = document.createElement('div');
+v1.className = "gallery";
+v2.innerHTML = "CUTE PINAY QUICKY SEX";
+v2.className = "desc";
+var a = document.createElement("a");
+a.setAttribute("href", "http://festyy.com/wXTPIW");
+
+var img = document.createElement("img");
+img.setAttribute('src', "https://img-l3.xnxx-cdn.com/videos/thumbslll/8b/29/01/8b290109b939e1fdc4944cfd8b146c78/8b290109b939e1fdc4944cfd8b146c78.12.jpg");
+
+a.appendChild(img);
+a.appendChild(v2);
+v1.appendChild(a);
+parent.appendChild(v1);
